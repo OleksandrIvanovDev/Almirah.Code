d13e8899429c0ac776f5d1c890445d2aa8ea2be4
Oleksandr Ivanov <oleksandr.ivanov.development@gmail.com>
2024-06-23 19:50:25 +0300

    Ref: Align source code with rubocop

--- lib/almirah/doc_items/heading.rb
+++ lib/almirah/doc_items/heading.rb
@@ -1,98 +1,96 @@
-require_relative "paragraph"
+# frozen_string_literal: true
+
+require_relative 'paragraph'
 
 class Heading < Paragraph
+  attr_accessor :level, :anchor_id, :section_number
 
-    attr_accessor :level
-    attr_accessor :anchor_id
-    attr_accessor :section_number
+  @@global_section_number = ''
 
-    @@global_section_number = ""
+  def initialize(doc, text, level)
+    super(doc, text)
+    @level = level
 
-    def initialize(doc, text, level)
-        @parent_doc = doc
-        @text = text
-        @level = level
-
-        if level != 0 # skip Doc Title
-            if @@global_section_number == ""
-                @@global_section_number = "1"
-                for n in 1..(level-1) do
-                    @@global_section_number += ".1"
-                end
-            else
-                previous_level = @@global_section_number.split(".").length
-
-                if previous_level == level
-
-                    a = @@global_section_number.split(".")
-                    a[-1] = (a[-1].to_i() +1).to_s
-                    @@global_section_number = a.join(".")
-
-                elsif level > previous_level
-
-                    a = @@global_section_number.split(".")
-                    a.push("1")
-                    @@global_section_number = a.join(".")
-                
-                else # level < previous_level
-
-                    a = @@global_section_number.split(".")
-                    delta = previous_level - level
-                    a.pop(delta)
-                    @@global_section_number = a.join(".")
-                    # increment
-                    a = @@global_section_number.split(".")
-                    a[-1] = (a[-1].to_i() +1).to_s
-                    @@global_section_number = a.join(".")
-                end
-            end
+    if level != 0 # skip Doc Title
+      if @@global_section_number == ''
+        @@global_section_number = '1'
+        (1..(level - 1)).each do |_n|
+          @@global_section_number += '.1'
         end
-        @section_number = @@global_section_number
-        @anchor_id = get_anchor_text()
-    end
+      else
+        previous_level = @@global_section_number.split('.').length
 
-    def get_section_info
-        if level == 0 # Doc Title
-            s = @text
-        else
-            s = @section_number + " " + @text
+        if previous_level == level
+
+          a = @@global_section_number.split('.')
+          a[-1] = (a[-1].to_i + 1).to_s
+          @@global_section_number = a.join('.')
+
+        elsif level > previous_level
+
+          a = @@global_section_number.split('.')
+          a.push('1')
+          @@global_section_number = a.join('.')
+
+        else # level < previous_level
+
+          a = @@global_section_number.split('.')
+          delta = previous_level - level
+          a.pop(delta)
+          @@global_section_number = a.join('.')
+          # increment
+          a = @@global_section_number.split('.')
+          a[-1] = (a[-1].to_i + 1).to_s
+          @@global_section_number = a.join('.')
         end
+      end
     end
+    @section_number = @@global_section_number
+    @anchor_id = get_anchor_text
+  end
 
-    def get_anchor_text()
-        s = @section_number + "-" + getTextWithoutSpaces()
+  def get_section_info
+    if level.zero? # Doc Title
+      @text
+    else
+      "#{@section_number} #{@text}"
     end
+  end
 
-    def to_html
-        s = ''
-        if @@html_table_render_in_progress
-            s += "</table>\n"
-            @@html_table_render_in_progress = false
-        end
-        heading_level = level.to_s
-        heading_text = get_section_info()
-        if level == 0
-            heading_level = 1.to_s  # Render Doc Title as a regular h1
-            heading_text = @text    # Doc Title does not have a section number
-        end 
-        s += "<a name=\"#{@anchor_id}\"></a>\n"
-        s += "<h#{heading_level}> #{heading_text} <a href=\"\##{@anchor_id}\" class=\"heading_anchor\">"
-        s += "&para;</a></h#{heading_level}>"
-        return s
-    end
+  def get_anchor_text
+    "#{@section_number}-#{getTextWithoutSpaces}"
+  end
 
-    def get_html_link
-        if (@parent_doc.instance_of? Specification)
-            heading_text = get_section_info()
-            s = "<a href= class=\"external\">#{heading_text}</a>"
-        end
+  def to_html
+    s = ''
+    if @@html_table_render_in_progress
+      s += "</table>\n"
+      @@html_table_render_in_progress = false
     end
+    heading_level = level.to_s
+    heading_text = get_section_info
+    if level.zero?
+      heading_level = 1.to_s # Render Doc Title as a regular h1
+      heading_text = @text    # Doc Title does not have a section number
+    end
+    s += "<a name=\"#{@anchor_id}\"></a>\n"
+    s += "<h#{heading_level}> #{heading_text} <a href=\"\##{@anchor_id}\" class=\"heading_anchor\">"
+    s += "&para;</a></h#{heading_level}>"
+    s
+  end
 
-    def get_url
-        "./specifications/#{parent_doc.id}/#{parent_doc.id}.html\##{@anchor_id}"
+  def get_html_link
+    if @parent_doc.instance_of? Specification
+      heading_text = get_section_info
+      "<a href= class=\"external\">#{heading_text}</a>"
     end
+  end
 
-    def self.reset_global_section_number
-        @@global_section_number = ""
-    end
+  def get_url
+    "./specifications/#{parent_doc.id}/#{parent_doc.id}.html\##{@anchor_id}"
+  end
+
+  def self.reset_global_section_number
+    @@global_section_number = ''
+  end
 end
