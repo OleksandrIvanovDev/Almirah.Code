173b799b3046e004a2ffea20dcae5dcb307f2b4e
Oleksandr Ivanov <oleksandr.ivanov.development@gmail.com>
2024-06-27 20:02:07 +0300

    Fix: Undefined behavior without document title (#102)

--- lib/almirah/dom/document.rb
+++ lib/almirah/dom/document.rb
@@ -3,8 +3,6 @@ require_relative 'doc_section'
 class Document
   attr_accessor :root_section
 
-  @@sections_stack = []
-
   def initialize(headings_list)
     @root_section = nil
 
@@ -12,47 +10,48 @@ class Document
   end
 
   def build_sections_tree(headings_list)
+    sections_stack = []
     headings_list.each do |h|
       if @root_section.nil?
 
         s = DocSection.new(h)
         s.parent_section = nil
         @root_section = s
-        @@sections_stack.push s
+        sections_stack.push s
         # one more artificial section copy if root is not a Document Title (level 0)
         if h.level.positive?
           a = DocSection.new(h)
           a.parent_section = @root_section
           @root_section.sections.append(a)
-          @@sections_stack.push a
+          sections_stack.push a
         end
 
-      elsif @@sections_stack[-1].heading.level == h.level
+      elsif sections_stack[-1].heading.level == h.level
 
         s = DocSection.new(h)
-        s.parent_section = @@sections_stack[-1].parent_section
-        @@sections_stack[-1].parent_section.sections.append(s)
-        @@sections_stack[-1] = s
+        s.parent_section = sections_stack[-1].parent_section
+        sections_stack[-1].parent_section.sections.append(s)
+        sections_stack[-1] = s
 
-      elsif h.level > @@sections_stack[-1].heading.level
+      elsif h.level > sections_stack[-1].heading.level
 
         s = DocSection.new(h)
-        s.parent_section = @@sections_stack[-1]
-        @@sections_stack[-1].sections.append(s)
-        @@sections_stack.push s
+        s.parent_section = sections_stack[-1]
+        sections_stack[-1].sections.append(s)
+        sections_stack.push s
 
       else
-        @@sections_stack.pop while h.level < @@sections_stack[-1].heading.level
-        @@sections_stack.push @root_section if @@sections_stack.empty?
+        sections_stack.pop while h.level < sections_stack[-1].heading.level
+        sections_stack.push @root_section if sections_stack.empty?
         s = DocSection.new(h)
-        if h.level == @@sections_stack[-1].heading.level
-          s.parent_section = @@sections_stack[-1].parent_section
-          @@sections_stack[-1].parent_section.sections.append(s)
+        if h.level == sections_stack[-1].heading.level
+          s.parent_section = sections_stack[-1].parent_section
+          sections_stack[-1].parent_section.sections.append(s)
         else
-          s.parent_section = @@sections_stack[-1]
-          @@sections_stack[-1].sections.append(s)
+          s.parent_section = sections_stack[-1]
+          sections_stack[-1].sections.append(s)
         end
-        @@sections_stack[-1] = s
+        sections_stack[-1] = s
       end
     end
   end
