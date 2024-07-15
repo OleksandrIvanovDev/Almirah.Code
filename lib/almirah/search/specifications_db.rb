0f804a4f6e081e057830df5d16e9b9f8c168727b
Oleksandr Ivanov <oleksandr.ivanov.development@gmail.com>
2024-07-15 16:04:10 +0300

    Ref: Align source code with rubocop (#90)

--- lib/almirah/search/specifications_db.rb
+++ lib/almirah/search/specifications_db.rb
@@ -1,83 +1,79 @@
+# frozen_string_literal: true
+
 require 'json'
+
 # Prepare JSON database file for further indexing by other tools
 class SpecificationsDb
+  attr_accessor :specifications, :data
 
-    attr_accessor :specifications
-    attr_accessor :data
+  def initialize(spec_list)
+    @specifications = spec_list
+    @data = []
+    create_data
+  end
 
-    def initialize(spec_list)
-        @specifications = spec_list
-        @data = []
-        create_data
-    end
-
-    def create_data
-        @specifications.each do |sp|
-            sp.items.each do |i|
-                if (i.instance_of? Paragraph) or (i.instance_of? ControlledParagraph)
-                    e = {"document" => i.parent_doc.title, \
-                        "doc_color" => i.parent_doc.color, \
-                        "text" => i.text, \
-                        "heading_url" => i.parent_heading.get_url(), \
-                        "heading_text" => i.parent_heading.get_section_info()
-                    }
-                    @data.append e                    
-                elsif i.instance_of? MarkdownList
-                    e = add_markdown_list_item_to_db( @data, i, i)
-                elsif i.instance_of? MarkdownTable
-                    add_markdown_table_item_to_db( @data, i, i)    
-                end
-            end
+  def create_data # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+    @specifications.each do |sp|
+      sp.items.each do |i|
+        if (i.instance_of? Paragraph) or (i.instance_of? ControlledParagraph)
+          e = { 'document' => i.parent_doc.title, \
+                'doc_color' => i.parent_doc.color, \
+                'text' => i.text, \
+                'heading_url' => i.parent_heading.get_url, \
+                'heading_text' => i.parent_heading.get_section_info }
+          @data.append e
+        elsif i.instance_of? MarkdownList
+          add_markdown_list_item_to_db(@data, i, i)
+        elsif i.instance_of? MarkdownTable
+          add_markdown_table_item_to_db(@data, i, i)
         end
+      end
     end
+  end
 
-    def add_markdown_list_item_to_db(data, item_for_reference, item_to_process)
-        e = nil
-        item_to_process.rows.each do |r|
-            if r.is_a?(MarkdownList)
-                f_text = r.text
-                e = {   "document" => item_for_reference.parent_doc.title, \
-                        "doc_color" => item_for_reference.parent_doc.color, \
-                        "text" => f_text, \
-                        "heading_url" => item_for_reference.parent_heading.get_url(), \
-                        "heading_text" => item_for_reference.parent_heading.get_section_info()
-                    }
-                data << e
-                add_markdown_list_item_to_db(data, item_for_reference, r)
-            else
-                f_text = r
-                e = {   "document" => item_for_reference.parent_doc.title, \
-                        "doc_color" => item_for_reference.parent_doc.color, \
-                        "text" => f_text, \
-                        "heading_url" => item_for_reference.parent_heading.get_url(), \
-                        "heading_text" => item_for_reference.parent_heading.get_section_info()
-                    }
-                data << e
-            end
-        end
-        return e
-    end
-
-    def add_markdown_table_item_to_db(data, item_for_reference, item_to_process)
-        e = nil
-        table_text = ""
-        item_to_process.rows.each do |row|
-            table_text += "| " + row.join(" | ") + " |"
-        end
-        e = {   "document" => item_for_reference.parent_doc.title, \
-                "doc_color" => item_for_reference.parent_doc.color, \
-                "text" => table_text, \
-                "heading_url" => item_for_reference.parent_heading.get_url(), \
-                "heading_text" => item_for_reference.parent_heading.get_section_info()
-            }
+  def add_markdown_list_item_to_db(data, item_for_reference, item_to_process) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+    e = nil
+    item_to_process.rows.each do |r|
+      if r.is_a?(MarkdownList)
+        f_text = r.text
+        e = {   'document' => item_for_reference.parent_doc.title, \
+                'doc_color' => item_for_reference.parent_doc.color, \
+                'text' => f_text, \
+                'heading_url' => item_for_reference.parent_heading.get_url, \
+                'heading_text' => item_for_reference.parent_heading.get_section_info }
         data << e
+        add_markdown_list_item_to_db(data, item_for_reference, r)
+      else
+        f_text = r
+        e = {   'document' => item_for_reference.parent_doc.title, \
+                'doc_color' => item_for_reference.parent_doc.color, \
+                'text' => f_text, \
+                'heading_url' => item_for_reference.parent_heading.get_url, \
+                'heading_text' => item_for_reference.parent_heading.get_section_info }
+        data << e
+      end
     end
+    e
+  end
 
-    def save(path)
-        json = JSON.generate(@data)
-
-        file = File.open( path + "/specifications_db.json", "w" )
-        file.puts json
-        file.close
+  def add_markdown_table_item_to_db(data, item_for_reference, item_to_process)
+    table_text = ''
+    item_to_process.rows.each do |row|
+      table_text += "| #{row.join(' | ')} |"
     end
+    e = {   'document' => item_for_reference.parent_doc.title, \
+            'doc_color' => item_for_reference.parent_doc.color, \
+            'text' => table_text, \
+            'heading_url' => item_for_reference.parent_heading.get_url, \
+            'heading_text' => item_for_reference.parent_heading.get_section_info }
+    data << e
+  end
+
+  def save(path)
+    json = JSON.generate(@data)
+
+    file = File.open("#{path}/specifications_db.json", 'w')
+    file.puts json
+    file.close
+  end
 end
