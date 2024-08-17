254274d33ef98810cff82cc4e70119cca9897d84
Oleksandr.Ivanov <Oleksandr.Ivanov.Dev@gmail.com>
2024-08-17 12:45:53 +0300

    Align source code with rubocop (#90)

--- lib/almirah/doc_types/coverage.rb
+++ lib/almirah/doc_types/coverage.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require_relative 'base_document'
 
-class Coverage < BaseDocument
+class Coverage < BaseDocument # rubocop:disable Style/Documentation
   attr_accessor :top_doc, :bottom_doc, :covered_items, :passed_steps_number, :failed_steps_number
 
   def initialize(top_doc)
@@ -8,18 +10,18 @@ class Coverage < BaseDocument
     @top_doc = top_doc
     @bottom_doc = nil
 
-    @id = top_doc.id + '-' + 'tests'
-    @title = 'Coverage Matrix: ' + @id
+    @id = "#{top_doc.id}-tests"
+    @title = "Coverage Matrix: #{@id}"
     @covered_items = {}
     @passed_steps_number = 0
     @failed_steps_number = 0
   end
 
   def to_console
-    puts "\e[35m" + 'Traceability: ' + @id + "\e[0m"
+    puts "\e[35mTraceability: #{@id}\e[0m"
   end
 
-  def to_html(nav_pane, output_file_path)
+  def to_html(nav_pane, output_file_path) # rubocop:disable Metrics/MethodLength
     html_rows = []
 
     html_rows.append('')
@@ -28,7 +30,7 @@ class Coverage < BaseDocument
     s += "\t<thead> <th>#</th> <th style='font-weight: bold;'>#{@top_doc.title}</th> <th>#</th> <th style='font-weight: bold;'>Test CaseId.StepId</th> </thead>\n"
     html_rows.append s
 
-    sorted_items = @top_doc.controlled_items.sort_by { |w| w.id }
+    sorted_items = @top_doc.controlled_items.sort_by(&:id)
 
     sorted_items.each do |top_item|
       row = render_table_row top_item
@@ -39,7 +41,7 @@ class Coverage < BaseDocument
     save_html_to_file(html_rows, nav_pane, output_file_path)
   end
 
-  def render_table_row(top_item)
+  def render_table_row(top_item) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
     s = ''
     if top_item.coverage_links
       id_color = if top_item.coverage_links.length > 1
@@ -52,10 +54,11 @@ class Coverage < BaseDocument
         s += "\t\t<td class=\"item_id\" #{id_color}><a href=\"./../#{top_item.parent_doc.id}/#{top_item.parent_doc.id}.html##{top_item.id}\" class=\"external\">#{top_item.id}</a></td>\n"
         s += "\t\t<td class=\"item_text\" style='width: 42%;'>#{top_item.text}</td>\n"
 
-        test_step_color = if bottom_item.columns[-2].text.downcase == 'pass'
+        test_step_color = case bottom_item.columns[-2].text.downcase
+                          when 'pass'
                             @passed_steps_number += 1
                             "style='background-color: #cfc;'"
-                          elsif bottom_item.columns[-2].text.downcase == 'fail'
+                          when 'fail'
                             @failed_steps_number += 1
                             "style='background-color: #fcc;'"
                           else
