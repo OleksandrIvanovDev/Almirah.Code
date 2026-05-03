4576cd7c95a9f1e584007abbe33e0b1333811eb4
AiAgent.Oleksandr <oleksandr.ivanov.development@gmail.com>
2026-05-03 14:45:39 +0200

    Test: for SRS-005 (#169)

---
+++ spec/e2e/statistics_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+
+RSpec.describe 'Statistics', type: :aruba do
+  before do
+    write_file('myproject/project.yml', <<~YML)
+      specifications:
+        input: []
+    YML
+    write_file('myproject/specifications/req/req.md', <<~MD)
+      # Requirements
+
+      [REQ-001] Controlled item #1.
+
+      [REQ-002] Controlled item #2.
+
+      Non-controlled item in REQ specification.
+
+    MD
+    write_file('myproject/specifications/arch/arch.md', <<~MD)
+      # Architecture
+
+      [ARCH-001] Controlled item with external reference from a Controlled Item to external Controlled Item >[REQ-001]
+
+      Non-controlled item in ARCH specification.
+      
+    MD
+    run_command_and_stop('almirah please myproject')
+  end
+
+  # <REQ> The software shall provide the "Number of Controlled Items" for each specification ID >[SRS-005] </REQ>
+  describe 'Number of Controlled Items' do
+    let(:idx) { Nokogiri::HTML(File.read(expand_path('myproject/build/index.html'))) }
+
+    it 'renders 2 items for REQ' do
+      cell = idx.at_xpath('//tr[.//a[contains(@href,"specifications/req/req.html")]]//td[@class="item_id"]')
+      expect(cell).not_to be_nil
+      expect(cell.text.strip).to eq('2')
+    end
+
+    it 'renders 1 item for ARCH' do
+      cell = idx.at_xpath('//tr[.//a[contains(@href,"specifications/arch/arch.html")]]//td[@class="item_id"]')
+      expect(cell).not_to be_nil
+      expect(cell.text.strip).to eq('1')
+    end
+  end
+end
